079fae6416c50982a752bbec5cf362ec29994b66
Juan Antonio Breña Moral <bren@juanantonio.info>
2021-11-21 18:02:33 +0100

    Adding examples about Context Mapper

---
+++ context-mapper-examples/src/test/java/org/contextmapper/standalone/example/ContextMapGeneratorExampleTest.java
@@ -0,0 +1,32 @@
+package org.contextmapper.standalone.example;
+
+import org.contextmapper.dsl.cml.CMLResource;
+import org.contextmapper.dsl.generator.ContextMapGenerator;
+import org.contextmapper.dsl.standalone.ContextMapperStandaloneSetup;
+import org.contextmapper.dsl.standalone.StandaloneContextMapperAPI;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ContextMapGeneratorExampleTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "./src/main/cml/my-examples/Event-Storming-Stage-1.cml",
+            "./src/main/cml/my-examples/Event-Storming-Stage-2.cml",
+            "./src/main/cml/my-examples/Event-Storming-Stage-3.cml",
+            "./src/main/cml/my-examples/Event-Storming-Stage-4.cml"
+    })
+    public void given_cml1_when_execute_then_Ok(String DSL_EXAMPLE_FILE) {
+
+        // Setup and loading CML file:
+        StandaloneContextMapperAPI contextMapper = ContextMapperStandaloneSetup.getStandaloneAPI();
+        //CMLResource resource = contextMapper.loadCML(INSURANCE_EXAMPLE_URI);
+        CMLResource resource = contextMapper.loadCML(DSL_EXAMPLE_FILE);
+
+        // Create the PlantUML generator
+        ContextMapGenerator generator = new ContextMapGenerator();
+
+        // Generate the diagrams into 'src-gen'
+        contextMapper.callGenerator(resource, generator);
+    }
+}
